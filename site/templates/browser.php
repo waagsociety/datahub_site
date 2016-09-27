f0cdc70f67c3019465c3ed92f60a9655bc4d1eaa
Martin Risseeuw <martin@martinr.nl>
2016-09-27 10:50:10 +0200

    implementing browser into kirby

--- site/templates/browser.php
+++ site/templates/browser.php
@@ -1,11 +1,11 @@
 <?php snippet('header') ?>
-<link rel="stylesheet" type="text/css" href="../assets/color.css">
-<link rel="stylesheet" type="text/css" href="../assets/style.css">
-<link rel="stylesheet" type="text/css" href="../assets/results.css">    
-<link rel="stylesheet" type="text/css" href="../assets/dataset.css">
-<link rel="stylesheet" type="text/css" href="../assets/mapbox.css">
+<link rel="stylesheet" type="text/css" href="browse-data/assets/color.css">
+<link rel="stylesheet" type="text/css" href="browse-data/assets/style.css">
+<link rel="stylesheet" type="text/css" href="browse-data/assets/results.css">    
+<link rel="stylesheet" type="text/css" href="browse-data/assets/dataset.css">
+<link rel="stylesheet" type="text/css" href="browse-data/assets/mapbox.css">
 <script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-supported/v1.2.0/mapbox-gl-supported.js'></script>
 <script src="https://use.fontawesome.com/23e6b28fc5.js"></script>
 <div id="app"></div>
-<script src="app.min.js"></script>
+<script src="browse-data/app.min.js"></script>
 <?php snippet('browser-footer') ?>
