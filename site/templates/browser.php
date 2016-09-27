e0e8be69044bca11bf25819283c03c1955a6dd51
Martin Risseeuw <martin@martinr.nl>
2016-09-27 15:13:10 +0200

    updated kirby links

--- site/templates/browser.php
+++ site/templates/browser.php
@@ -1,11 +1,13 @@
 <?php snippet('header') ?>
-<link rel="stylesheet" type="text/css" href="browse-data/assets/color.css">
-<link rel="stylesheet" type="text/css" href="browse-data/assets/style.css">
-<link rel="stylesheet" type="text/css" href="browse-data/assets/results.css">    
-<link rel="stylesheet" type="text/css" href="browse-data/assets/dataset.css">
-<link rel="stylesheet" type="text/css" href="browse-data/assets/mapbox.css">
+<?php echo css(array(
+  'assets/css/color.css',
+  'assets/css/style.css',
+  'assets/css/results.css',
+  'assets/css/dataset.css',
+  'assets/css/mapbox.css'
+)) ?>
 <script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-supported/v1.2.0/mapbox-gl-supported.js'></script>
 <script src="https://use.fontawesome.com/23e6b28fc5.js"></script>
 <div id="app"></div>
-<script src="browse-data/app.min.js"></script>
+<?php echo js('assets/js/app.min.js') ?>
 <?php snippet('browser-footer') ?>
