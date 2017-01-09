bdf5d420e3c695b77ec3d3b3cb4d9eee3128175a
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2017-01-09 16:42:54 +0100

    restored projects link

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -12,7 +12,7 @@
     </li>
     <?php endforeach ?>
     <li class="main__navigation__list__item ">
-      <a href="http://amsdatahub.waag.org/browser#type=project" title="Link to AMS website">Projects</a>
+      <a href="http://amsdatahub.waag.org/browser#search=*&type=project" title="Link to AMS website">Projects</a>
     </li>
   </ul>
 </nav>
